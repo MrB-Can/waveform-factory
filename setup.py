7af86ce7de79216906b9dcfc9a817788340f7073
MrB-Can <paul@bigeye.com>
2023-07-24 14:07:19 -0700

    Updated description vs long description closes #4

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ setup(
     url='https://github.com/MrB-Can/waveform-factory',
     author='Paul Bennett',
     author_email='paul@lgis.ca',
-    description='Waveform Factory is a python library that generates various types of waveform patterns. It is '
+    description='Wave form data generator',
+    lonmg_description='Waveform Factory is a python library that generates various types of waveform patterns. It is '
                 'designed to aid in the creation, manipulation, and analysis of waveform data for various '
                 'applications including music, signal processing, and more.',
     packages=find_packages(),
