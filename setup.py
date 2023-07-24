e7619bead2e151fea4a29caeedb7f1ce682a4480
Paul Bennett <114028989+MrB-Can@users.noreply.github.com>
2023-07-24 15:19:55 -0700

    Packaging v 0.1.3 (#8)
    * Square wave completed
    Added the pattern and tests for square wave. Closes #2
    
    * Delete hana-req.txt
    
    Accidentally committed to the wro
    ng place.
    
    * Packaged and uploaded
    Uploaded and available at https://pypi.org/project/waveform-factory/0.1.0/
    
    * Updated description vs long description
    closes #4
    
    * Update README.md
    
    Added PyPI badge
    
    * Enhancement/setup (#7)
    
    * Package 0.1.1
    
    * Updated setup.py and added scipy dependency
    
    * Finalize requirements.txt

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
-    name='waveform-factory',
-    version='0.1.0',
+    name='waveform_factory',
+    version='0.1.1',
     url='https://github.com/MrB-Can/waveform-factory',
     author='Paul Bennett',
     author_email='paul@lgis.ca',
     description='Wave form data generator',
-    lonmg_description='Waveform Factory is a python library that generates various types of waveform patterns. It is '
+    long_description='Waveform Factory is a python library that generates various types of waveform patterns. It is '
                 'designed to aid in the creation, manipulation, and analysis of waveform data for various '
                 'applications including music, signal processing, and more.',
-    packages=find_packages(),
-    install_requires=[
-        'numpy',
-        'matplotlib',
-    ],
+    packages=find_packages(include=['waveform_factory', 'waveform_factory.*']),
+    install_requires=required,
 )
+ 
